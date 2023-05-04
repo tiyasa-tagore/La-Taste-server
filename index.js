ff1633139f993efabac73d58afec7e5fa76182a6
tiyasa-tagore <tiyasatagore1999@gmail.com>
2023-05-05 01:47:07 +0600

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Chef recipe hunter is running...')
