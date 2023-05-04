cdb56dbf16b9d45d7067ece8f748bb95a3d1c1bb
tiyasa-tagore <tiyasatagore1999@gmail.com>
2023-05-05 02:18:32 +0600

    dynamic

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Chef recipe hunter is running...')
 })
+app.get('/all', (req, res) => {
+    res.send(chefs)
+})
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id
+    const singleChef = chefs.find(chef => chef.id == id)
+    res.send(singleChef)
+})
+
 app.listen(port, () => {
     console.log(`Chef recipe hunter is running on port ${port}`)
 })
